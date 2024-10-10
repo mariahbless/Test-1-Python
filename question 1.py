35b685762be06c27ec3b1933c226e529220fe12e
Akello mary <akellomary665@gmail.com>
2024-10-10 05:10:34 -0700

    tests1

---
+++ question 1.py
@@ -0,0 +1,6 @@
+#Question 1(a)
+#Create aprogram that calculates the area of a triangle
+base = int(input("Enter the base here:"))
+height = int(input("Enter the height here:"))
+area = 1/2 * (base * height)
+print(area)
---
+++ question 2.py
@@ -0,0 +1,6 @@
+#Quetion 2(a)
+#Using a function, create a program tha calculates the volume of a cylinder
+radius = int(input("Enter the radius here:"))
+height = int(input("Enter the height here:"))
+volume = 22/7 * (radius**2 * height)
+print(volume)
